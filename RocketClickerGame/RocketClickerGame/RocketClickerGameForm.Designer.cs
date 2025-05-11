c36a47c6d11b9c565915690fab4e2a90091824ee
MEERABLK <meerabmercury@outlook.com>
2025-05-10 22:49:45 -0400

    default active play style added for clicking and counting time

--- RocketClickerGame/RocketClickerGame/RocketClickerGameForm.Designer.cs
+++ RocketClickerGame/RocketClickerGame/RocketClickerGameForm.Designer.cs
@@ -28,7 +28,12 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
             this.rocketButton1 = new System.Windows.Forms.Button();
+            this.timeLabel = new System.Windows.Forms.Label();
+            this.clicksLabel = new System.Windows.Forms.Label();
+            this.timer = new System.Windows.Forms.Timer(this.components);
+            this.restartButton = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // rocketButton1
@@ -46,6 +51,53 @@
             this.rocketButton1.Size = new System.Drawing.Size(192, 421);
             this.rocketButton1.TabIndex = 0;
             this.rocketButton1.UseVisualStyleBackColor = false;
+            this.rocketButton1.Click += new System.EventHandler(this.rocketButton1_Click);
+            // 
+            // timeLabel
+            // 
+            this.timeLabel.AutoSize = true;
+            this.timeLabel.BackColor = System.Drawing.Color.Transparent;
+            this.timeLabel.Font = new System.Drawing.Font("Bahnschrift SemiCondensed", 26.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.timeLabel.ForeColor = System.Drawing.Color.White;
+            this.timeLabel.Location = new System.Drawing.Point(21, 9);
+            this.timeLabel.Name = "timeLabel";
+            this.timeLabel.Size = new System.Drawing.Size(113, 42);
+            this.timeLabel.TabIndex = 1;
+            this.timeLabel.Text = "Time: 0";
+            // 
+            // clicksLabel
+            // 
+            this.clicksLabel.AutoSize = true;
+            this.clicksLabel.BackColor = System.Drawing.Color.Transparent;
+            this.clicksLabel.Font = new System.Drawing.Font("Bahnschrift SemiCondensed", 26.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.clicksLabel.ForeColor = System.Drawing.Color.White;
+            this.clicksLabel.Location = new System.Drawing.Point(21, 51);
+            this.clicksLabel.Name = "clicksLabel";
+            this.clicksLabel.Size = new System.Drawing.Size(131, 42);
+            this.clicksLabel.TabIndex = 2;
+            this.clicksLabel.Text = "Clicks: 0";
+            // 
+            // timer
+            // 
+            this.timer.Interval = 1000;
+            this.timer.Tick += new System.EventHandler(this.timer_Tick);
+            // 
+            // restartButton
+            // 
+            this.restartButton.BackColor = System.Drawing.Color.Transparent;
+            this.restartButton.BackgroundImage = global::RocketClickerGame.Properties.Resources.Restart;
+            this.restartButton.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.restartButton.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.restartButton.FlatAppearance.BorderSize = 0;
+            this.restartButton.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.restartButton.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.restartButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.restartButton.Location = new System.Drawing.Point(1010, 12);
+            this.restartButton.Name = "restartButton";
+            this.restartButton.Size = new System.Drawing.Size(58, 61);
+            this.restartButton.TabIndex = 3;
+            this.restartButton.UseVisualStyleBackColor = false;
+            this.restartButton.Click += new System.EventHandler(this.restartButton_Click);
             // 
             // RocketClickerGameForm
             // 
@@ -54,17 +106,26 @@
             this.BackgroundImage = global::RocketClickerGame.Properties.Resources.centaurbridgeRender;
             this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
             this.ClientSize = new System.Drawing.Size(1080, 608);
+            this.Controls.Add(this.restartButton);
+            this.Controls.Add(this.clicksLabel);
+            this.Controls.Add(this.timeLabel);
             this.Controls.Add(this.rocketButton1);
             this.DoubleBuffered = true;
             this.Name = "RocketClickerGameForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Rocket Factory Tycoon";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
         private System.Windows.Forms.Button rocketButton1;
+        private System.Windows.Forms.Label timeLabel;
+        private System.Windows.Forms.Label clicksLabel;
+        private System.Windows.Forms.Timer timer;
+        private System.Windows.Forms.Button restartButton;
     }
 }
 
--- RocketClickerGame/RocketClickerGame/RocketClickerGameForm.cs
+++ RocketClickerGame/RocketClickerGame/RocketClickerGameForm.cs
@@ -12,11 +12,55 @@ namespace RocketClickerGame
 {
     public partial class RocketClickerGameForm : Form
     {
+        private int ticks;
+
+        private int clicks;
+
+        
         public RocketClickerGameForm()
         {
             InitializeComponent();
         }
 
-     
+
+        private void rocketButton1_Click(object sender, EventArgs e)
+        {
+            //rocket timer starts at click
+            timer.Start();
+
+            // displays as int number of clicks
+            clicksLabel.Text = "Clicks: " + clicks;
+
+            //the number of clicks increment at each click
+            clicks++;
+        }
+
+        //the time
+        private void timer_Tick(object sender, EventArgs e)
+        {
+            //timer
+            //increase by one second(1000 milisecond interval property equal to 1 second)
+            ticks++;
+
+            //displays the number of seconds
+            timeLabel.Text = "Time: " + ticks;
+
+
+            
+        }
+
+        //to restart the game
+        private void restartButton_Click(object sender, EventArgs e)
+        {
+            //the timer stops reset  number of seconds and clicks back to 0
+            timer.Stop();
+            clicks = 0;
+            ticks = 0;
+
+            //timer label back to initial state
+            timeLabel.Text = "Time: 0";
+            clicksLabel.Text = "Clicks: 0";
+
+        }
     }
 }
